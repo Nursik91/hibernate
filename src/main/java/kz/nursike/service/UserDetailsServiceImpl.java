5600386537a23b38cee82d7ee271ab7991ff1717
Нурсултан Ережепов <@corp.itstep>
2017-12-25 19:43:31 +0600

    first commit

---
+++ src/main/java/kz/nursike/service/UserDetailsServiceImpl.java
@@ -0,0 +1,9 @@
+package kz.nursike.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by ЕрежеповН on 25.12.2017.
+ */
+public class UserDetailsServiceImpl implements UserDetailsService {
+}
---
+++ src/main/java/kz/nursike/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package kz.nursike.service;
+
+import kz.nursike.model.User;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ЕрежеповН on 22.12.2017.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+}
